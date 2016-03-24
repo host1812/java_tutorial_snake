4c4aa2c25d837540aae174310d6f6bd0958cce72
Szabolcs Fruhwald <sfruhwald@google.com>
2016-03-23 18:10:12 -0700

    Trigger test codeship.com CI

---
+++ snake/src/test/java/org/foo/training/snake/GameRunnerTests.java
@@ -0,0 +1,13 @@
+package org.foo.training.snake;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GameRunnerTests {
+
+	@Test
+	public void testSomething() {
+		Assert.assertEquals(2, 5);
+	}
+	
+}
